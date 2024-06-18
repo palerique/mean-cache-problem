836162dfba6fde9eb5ae8ba17197cdbab2d497a6
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2024-06-18 13:02:38 -0300

    feat[api]: add cookie-parser and uuid dependencies and update problem solution
    Added the cookie-parser and uuid dependencies to the package.json and the lock file. Created a new dto for the Fingerprint and renamed the addRecord and cacheState dtos. The controller was updated to use the new dependencies and added records and calculated mean now do so by the fingerprint, ruling out the need for a solution in the request body. The service was updated with new methods to match the controller changes. Initialized the app to use the cookieParser.

--- apps/api/src/meancacheproblem/meancacheproblem.controller.ts
+++ apps/api/src/meancacheproblem/meancacheproblem.controller.ts
@@ -1,108 +1,71 @@
-import {
-    Body,
-    Controller,
-    HttpException,
-    HttpStatus,
-    Inject,
-    Logger,
-    Post,
-} from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post, Req, Res } from '@nestjs/common';
 import { MeanCacheProblemService } from './meanCacheProblem.service';
-import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
-import {
-    ApiBody,
-    ApiOperation,
-    ApiResponse,
-    ApiTags,
-    getSchemaPath,
-} from '@nestjs/swagger';
-import { NoPossibleSolutionDto } from './dto/noPossibleSolution.dto';
-import { CacheStateDto } from './dto/cacheStateDto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AddRecordDto } from './dto/addRecordDto';
+import { FingerprintDto } from './dto/Fingerprint.dto';
+import { v4 as uuidv4 } from 'uuid';
 
-@Controller('meanCacheProblem')
+@Controller('api')
 export class MeanCacheProblemController {
     private readonly logger = new Logger(MeanCacheProblemController.name);
 
-    constructor(
-        @Inject(CACHE_MANAGER) private cacheManager: Cache,
-        private readonly meanCacheService: MeanCacheProblemService,
-    ) {}
+    constructor(private readonly meanCacheService: MeanCacheProblemService) {}
 
-    @Post('solve')
-    @ApiOperation({ summary: 'Solve the mean cache problem' })
+    @Post('records')
+    @ApiOperation({ summary: 'Add a new record' })
     @ApiResponse({
-        status: 200,
-        description: 'Returns the solution to the mean cache problem',
-        type: CacheStateDto,
-        schema: {
-            example: {
-                solution: [],
-            },
-            $ref: getSchemaPath(CacheStateDto),
-        },
-    })
-    @ApiResponse({
-        status: 400,
-        description: 'Returns an error message if the input values are invalid',
-        type: NoPossibleSolutionDto,
-        schema: {
-            example: {
-                message: 'No solution is possible',
-            },
-            $ref: getSchemaPath(NoPossibleSolutionDto),
-        },
+        status: 201,
+        description: 'Record added successfully',
     })
     @ApiTags('mean-cache-problem')
     @ApiBody({ type: AddRecordDto })
-    async solve(
-        @Body() body: AddRecordDto,
-    ): Promise<CacheStateDto | NoPossibleSolutionDto> {
-        const { value } = body;
-        this.logger.log(`Received solve request with value=${value}`, value);
+    async addRecord(
+        @Req() req: any,
+        @Body() { value }: AddRecordDto,
+    ): Promise<void> {
+        const fingerprint = req.cookies['fingerprint'];
+        logger.log(
+            `Adding record with value: ${value} to the fingerprint: ${fingerprint}`,
+        );
+        this.meanCacheService.addRecord(fingerprint, value);
+    }
 
-        if (!value) {
-            this.logger.error(
-                'Invalid input values. Ensure value is provided.',
-                value,
-            );
-            throw new HttpException(
-                'Please provide value, y_capacity, and z_amount_wanted.',
-                HttpStatus.BAD_REQUEST,
-            );
+    @Get('mean')
+    @ApiOperation({ summary: 'Calculate the mean of the records' })
+    @ApiResponse({
+        status: 200,
+        description: 'Returns the mean of the records',
+    })
+    @ApiTags('mean-cache-problem')
+    async calculateMean(@Req() req: any): Promise<number> {
+        const fingerprint = req.cookies['fingerprint'];
+        logger.log(`Calculating mean for the fingerprint: ${fingerprint}`);
+        return this.meanCacheService.calculateMean(fingerprint);
+    }
+
+    @Post('initialization')
+    @ApiOperation({ summary: 'Initialize the service with a fingerprint' })
+    @ApiResponse({
+        status: 200,
+        description: 'Service initialized successfully',
+    })
+    @ApiTags('mean-cache-problem')
+    @ApiBody({ type: FingerprintDto })
+    async initialize(
+        @Body() body: FingerprintDto,
+        @Req() req: any,
+        @Res({ passthrough: true }) res: any,
+    ): Promise<void> {
+        let { fingerprint } = body;
+        fingerprint = req.cookies['fingerprint'] || fingerprint;
+        if (!fingerprint) {
+            fingerprint = uuidv4();
         }
-
-        const parsedValue = parseInt(value, 10);
-
-        if (isNaN(parsedValue)) {
-            this.logger.error('Invalid input values');
-            throw new HttpException(
-                'Invalid input value.',
-                HttpStatus.BAD_REQUEST,
-            );
-        }
-
-        // const cacheKey = `${parsedValue}-${y}-${z}`;
-        // const cachedSolution: string = await this.cacheManager.get(cacheKey);
-        //
-        // if (cachedSolution) {
-        //     this.logger.log('Returning cached solution', cachedSolution);
-        //     return { solution: JSON.parse(cachedSolution) };
-        // }
-
-        this.logger.log('No cached solution found. Calculating solution...');
-        const solution = this.meanCacheService.addRecord(parsedValue);
-        if (solution) {
-            this.logger.log('Solution found', solution);
-            // await this.cacheManager.set(
-            //     cacheKey,
-            //     JSON.stringify(solution),
-            //     1800000,
-            // );
-            return { solution };
-        } else {
-            this.logger.warn('No solution possible');
-            return { message: 'No solution possible' };
-        }
+        logger.log(`Initializing service with fingerprint: ${fingerprint}`);
+        this.meanCacheService.initialize(fingerprint);
+        res.cookie('fingerprint', fingerprint, {
+            httpOnly: true,
+            path: '/',
+        });
     }
 }
